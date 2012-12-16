e70129737dfff21920996b8ac1cc448362c674be
Quentin Bisson <bissoqu1@Quentin-PC.(none)>
2012-12-16 14:17:13 +0100

    Erreur expace début mvc

--- admin/vue /AccueilAdmin.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include_once("../Model/Entite/Medecin.php");
-	session_start();
-	echo "Admin";
-	$m = unserialize($_SESSION['user']);
-	echo "<h1>".$m->getLogin()."</h1>"
-	//echo "<h1>".$_SESSION['user']."</h1>"
-?>
