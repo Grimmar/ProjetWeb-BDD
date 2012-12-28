e8fd63e236c0aa75d0a8cc75dcda03ffc016c1bc
David <David@David-PC>
2012-12-28 13:39:12 +0100

    Erreur

--- models/Entite/Medecin.php
+++ models/Entite/Medecin.php
@@ -2,26 +2,32 @@
 
 require_once("Personne.php");
 
-class Medecin extends Personne{
-	
-	private $login;
-        private $motDePasse;
-	private $role;
-	
-	function __construct($login, $motDePasse, $role, $matricule, $nom, $prenom, $telephone, $numeroSecu, $dateNaissance, $adresse) {
-            parent::__construct($matricule, $nom, $prenom, $telephone, $numeroSecu, $dateNaissance, $adresse);
-            $this->login = $login;
-            $this->motDePasse = $motDePasse;
-            $this->role = $role;
-        }
+class Medecin extends Personne {
 
-                
-	public function getLogin(){
-		return $this->login;
-	}
-	
-	public function getRole(){
-		return $this->role;
-	}
+    private $login;
+    private $motDePasse;
+    private $role;
+
+    function __construct($login, $motDePasse, $role, $matricule, $nom, $prenom, $telephone, $numeroSecu, $dateNaissance, $adresse) {
+        parent::__construct($matricule, $nom, $prenom, $telephone, $numeroSecu, $dateNaissance, $adresse);
+        $this->login = $login;
+        $this->motDePasse = $motDePasse;
+        $this->role = $role;
+    }
+
+    public function getLogin() {
+        return $this->login;
+    }
+
+    public function getRole() {
+        return $this->role;
+    }
+    
+    public function getMotDePasse() {
+        return $this->motDePasse;
+    }
+
+
 }
+
 ?>
